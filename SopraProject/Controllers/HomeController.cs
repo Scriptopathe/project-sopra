70685fb09158d3e69d82d9821ebacc925357cdc6
Scriptopathe <josue.alvarez@hotmail.fr>
2015-12-01 20:06:37 +0100

    HomeController now renders the test page

--- SopraProject/Controllers/HomeController.cs
+++ SopraProject/Controllers/HomeController.cs
@@ -14,7 +14,7 @@ namespace SopraProject.Controllers
             var mvcName = typeof(Controller).Assembly.GetName();
             var isMono = Type.GetType("Mono.Runtime") != null;
 
-            return Content("Welcome home ! ");
+            return View("Index");
         }
     }
 }
