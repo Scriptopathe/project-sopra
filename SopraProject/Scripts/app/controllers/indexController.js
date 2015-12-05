6e2586cc84da3c9869cd9cdbde3bc93ba9a1b15d
Scriptopathe <josue.alvarez@hotmail.fr>
2015-12-05 11:54:15 +0100

    Added a test function for the web application.

--- SopraProject/Scripts/app/controllers/indexController.js
+++ SopraProject/Scripts/app/controllers/indexController.js
@@ -3,4 +3,36 @@
 function(serverService, $scope) {
 	$scope.server = serverService;
 	$scope.test = "Message depuis indexController " + $scope.server.serviceData;
+	$scope.rooms = []
+
+	// Fonction de test ! :p
+	$scope.updateRooms = function() 
+	{
+		$scope.server.getRessource("rooms", {})
+			.done(function(data, statusCode)
+			{
+				// On parse le XML qu'on a récupéré du serveur.
+				var xml = $( $.parseXML( data ) )
+
+				// On parcours tous les noeuds "Room"
+				xml.find("Room").each(function()
+				{
+					// On obtient une représentation du Noeud <Room>
+					var room = $(this);
+
+					// $scope.$apply permet de faire en sorte qu'angular
+					// force la vérification des modifications et mette à jour
+					// la vue une fois qu'on a modifié $scope.rooms !
+					$scope.$apply(function() 
+					{
+						// On prend l'attribut id de la room
+						var roomId = room.attr("id");
+						// On récupère le texte contenu dans le champ "Name"
+						var roomName = room.children("Name").text();
+						// On ajoute le tout dans $scope.rooms
+						$scope.rooms.push({ "id" : roomId, "name" : roomName });
+					});
+				});
+			});
+	}
 }]);
--- SopraProject/Scripts/app/services/serverService.js
+++ SopraProject/Scripts/app/services/serverService.js
@@ -1,3 +1,16 @@
 ﻿app.service("serverService", function() {
 	this.serviceData = "truc";
+
+	// Gets the ressource under api/ with the given partial URI and arguments.
+	// For instance : getRessource("rooms", { "id" : 1 }) will direct to 
+	// 				  www.domain.com/api/rooms?id=1
+	// Utilisation example :
+	// getRessource("Rooms, {})
+	//	.done(function(data, statusCode) { alert(data); });
+	//  .fail(...)			[ Optional ]
+	//  .always(...)		[ Optional ]
+	this.getRessource = function(partialUri, args)
+	{
+		return $.get("api/" + partialUri, args)
+	};
 });
