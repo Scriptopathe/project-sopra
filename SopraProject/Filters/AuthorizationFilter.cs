8db09c6e19aff7f58b82148a4f63ebdbab26b753
Scriptopathe <josue.alvarez@hotmail.fr>
2015-12-05 11:53:26 +0100

    Added an option to disable authentication check

--- SopraProject/Filters/AuthorizationFilter.cs
+++ SopraProject/Filters/AuthorizationFilter.cs
@@ -9,6 +9,14 @@ namespace SopraProject
     /// </summary>
     public class AuthorizationFilterAttribute : ActionFilterAttribute
     {
+        /// <summary>
+        /// Value indicating if authorization filtering is active.
+        /// This value must be set to true in the production environnment.
+        /// 
+        /// Set it to false to be able to debug programs quickly.
+        /// </summary>
+        public const bool ENABLE_FILTERING = false;
+
         public AuthorizationFilterAttribute()
         {
         }
@@ -16,12 +24,21 @@ namespace SopraProject
         public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
             base.OnActionExecuting(filterContext);
-            string authTicket = (string)filterContext.HttpContext.Session["AuthTicket"];
-            HttpCookie authCookie = filterContext.HttpContext.Request.Cookies["AuthTicket"];
-            if (authTicket == null || authCookie == null || authTicket != authCookie.Value)
+            if (ENABLE_FILTERING)
             {
-                // filterContext.Result = new ContentResult() { Content = "Authentication failed" };
-                filterContext.Result = new HttpStatusCodeResult(System.Net.HttpStatusCode.Unauthorized);
+                string authTicket = (string)filterContext.HttpContext.Session["AuthTicket"];
+                HttpCookie authCookie = filterContext.HttpContext.Request.Cookies["AuthTicket"];
+                if (authTicket == null || authCookie == null || authTicket != authCookie.Value)
+                {
+                    // filterContext.Result = new ContentResult() { Content = "Authentication failed" };
+                    filterContext.Result = new HttpStatusCodeResult(System.Net.HttpStatusCode.Unauthorized);
+                }
+            }
+            else
+            {
+                // Log in with a default user.
+                if(filterContext.HttpContext.Session["User"] == null)
+                    filterContext.HttpContext.Session["User"] = ObjectApi.User.Authenticate("User1", "User1pass");
             }
         }
     }
