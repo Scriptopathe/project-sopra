c24155d79a5deb736144fcf19b82e47abd45b9d6
Bex-Chauvet <olivier.bexchauvet@gmail.com>
2016-01-06 10:12:35 +0100

    Corrected rooms function

--- SopraProject/Models/ObjectAPI/SiteWithRooms.cs
+++ SopraProject/Models/ObjectAPI/SiteWithRooms.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using SopraProject.ObjectApi;
 
-namespace SopraProject
+namespace SopraProject.ObjectApi
 {
     public class SiteWithRooms
     {
+        [XmlAttribute("siteId")]
+        public string SiteId { get; set; }
+
+        [XmlArray("rooms")]
+        public List<Room> Rooms { get; set; }
+
         public SiteWithRooms()
         {
+            Rooms = new List<Room>();
         }
+
+        public SiteWithRooms(string siteId)
+        {
+            Rooms = new List<Room>(Site.Get(new SiteIdentifier(siteId)).Rooms);
+        }
+
     }
 }
 
