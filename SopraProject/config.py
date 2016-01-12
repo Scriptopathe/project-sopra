ba7204ee20322dd765b9ae74b698c68a244a567d
Scriptopathe <josue.alvarez@hotmail.fr>
2016-01-12 20:37:07 +0100

    Add script to switch configurations

---
+++ SopraProject/config.py
@@ -0,0 +1,30 @@
+import sys
+import shutil
+
+mode = input("mode [save | restore]: ")
+suffix = input("suffix: ")
+
+if mode == "save":
+	print("Copy current configuration to " + suffix)
+else:
+	print("Replace current configuration by " + suffix)
+
+confirm = input("type 'ok' then press enter to confirm: ")
+if(confirm != "ok"):
+	sys.exit()
+
+proj = "SopraProject.csproj"
+packages = "packages.config"
+web1 = "Web.config"
+web2 = "Views/Web.config"
+
+files = [proj, packages, web1, web2]
+
+if(mode == "save"):
+	for file in files:
+		shutil.copy(file, file + "." + suffix)
+else:
+	for file in files:
+		shutil.copy(file, file + ".backup")
+		shutil.copy(file + "." + suffix, file)
+		
